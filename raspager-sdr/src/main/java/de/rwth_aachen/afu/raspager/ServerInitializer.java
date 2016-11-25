e851cd25368332cb796ccfce71d9cd30be22d766
Philipp Thiel <pthiel@radicalworlds.de>
2016-11-25 09:50:55 +0100

    Removed SSL support from server initializer.

--- raspager-sdr/src/main/java/de/rwth_aachen/afu/raspager/ServerInitializer.java
+++ raspager-sdr/src/main/java/de/rwth_aachen/afu/raspager/ServerInitializer.java
@@ -7,7 +7,6 @@ import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.ssl.SslContext;
 
 /**
  * This class initializes a new socket channel when a connection is accepted.
@@ -17,7 +16,6 @@ final class ServerInitializer extends ChannelInitializer<SocketChannel> {
 	private static final StringEncoder encoder = new StringEncoder();
 	private static final StringDecoder decoder = new StringDecoder();
 	private final ServerHandler handler;
-	private final SslContext sslContext;
 
 	/**
 	 * Constructs a new server initializer instance.
@@ -26,29 +24,12 @@ final class ServerInitializer extends ChannelInitializer<SocketChannel> {
 	 *            Callback provider
 	 */
 	public ServerInitializer(ServerCallbacks callbacks) {
-		this(null, callbacks);
-	}
-
-	/**
-	 * Constructs a new server initializer instance.
-	 * 
-	 * @param sslContext
-	 *            SSL context to use.
-	 * @param callbacks
-	 *            Callback provider.
-	 */
-	public ServerInitializer(SslContext sslContext, ServerCallbacks callbacks) {
-		this.sslContext = sslContext;
 		this.handler = new ServerHandler(callbacks);
 	}
 
 	@Override
 	protected void initChannel(SocketChannel ch) throws Exception {
 		ChannelPipeline pip = ch.pipeline();
-
-		if (sslContext != null) {
-			pip.addLast(sslContext.newHandler(ch.alloc()));
-		}
 
 		pip.addLast(new DelimiterBasedFrameDecoder(4096, Delimiters.lineDelimiter()));
 		// Static as both encoder and decoder are sharable.
